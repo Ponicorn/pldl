f663f2714056fe3e24143ad4b50c04785866f405
Christophe Sauder <christophe.sauder@univ-lorraine.fr>
2018-11-28 09:25:40 +0100

    checkpoint

--- src/Cli.js
+++ src/Cli.js
@@ -8,24 +8,15 @@ async function app () {
   init()
   let result
 
+  // Ask user name and fetch playlist
   result = await askUsername()
   let playlists = await spotify.fetchPlaylists(result.username)
   if (!playlists || playlists.length == 0) return console.log('Oops ! Pas de playlist a l\'horizon !')
 
   result = await askPlaylist(playlists)
-  let songs = await spotify.fetchPlaylistItems(result.playlists)
-  if (!songs || songs.length == 0) return console.log('Oops ! ça va pas le faire on dirait...')
-
-  // TODO: Ici on a la playlist a dl, on doit 'juste' dl pour chaque morceau desormais.
-  // Rappel: pour dl,
-  //      1) trouver le morceau sur yt (nom/artist/temps)
-  //      2) telecharger le morceau si ok
-  //      3) convertir au bon format si besoin
-  //      4) ???
-  //      5) profit
-  songs.forEach(s => {
-    console.log(`${s.name} - ${s.artist}`)
-  })
+  let playlist = result.playlist
+  
+  console.log('see ya')
 }
 
 function init () {
@@ -56,10 +47,18 @@ function askPlaylist (playlists) {
   })
   return prompts({
     type: 'select',
-    name: 'playlists',
+    name: 'playlist',
     message: 'Choose a playlist: ',
     choices: pls
   })
 }
+
+// async function showTracks (playlist) {
+//   let songs = await spotify.fetchPlaylistItems(playlist)
+//   if (!songs || songs.length == 0) return console.log('Oops ! ça va pas le faire on dirait...')
+//   songs.forEach(s => {
+//     console.log(`${s.name} - ${s.artist}`)
+//   })
+// }
 
 module.exports = app
